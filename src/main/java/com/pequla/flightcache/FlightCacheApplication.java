329fae4faccdd968dae36bcb84eb399dfc7f876a
Petar Kresoja <petarkresoja5@gmail.com>
2022-11-22 10:17:10 +0100

    Initial upload

---
+++ src/main/java/com/pequla/flightcache/FlightCacheApplication.java
@@ -0,0 +1,13 @@
+package com.pequla.flightcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightCacheApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pequla/flightcache/FlightCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pequla.flightcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightCacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
